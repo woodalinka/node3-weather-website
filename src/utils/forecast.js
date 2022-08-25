d8f5b361576a94f2d1ea944643338e8847cd8309
Alina Woodcock <74838426+woodalinka@users.noreply.github.com>
2022-08-24 21:55:12 -0500

    Added wind info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require("request");
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=40688973e0ffae53b8d3edab51ef8d3d&query=' + latitude + ',' + longitude +'&units=f'
     request({url, json: true}, (error, {body}) => {
@@ -8,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The wind speed is ' + body.current.wind_speed + ' miles per hour.')
         }
     })
 };
